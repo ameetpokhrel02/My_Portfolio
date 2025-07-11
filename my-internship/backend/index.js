7a27cd1b1a2ce8c1b861fec1f66b8ae3cf47a383
your_github_username <your_github_email>
2025-07-11 15:09:32 +0545

    git backemd'

--- my-internship/backend/index.js
+++ my-internship/backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/my_internship_portfolio';
 
-// Middleware
+// Middleware for backend
 app.use(cors());
 app.use(express.json());
 
